8365a2c1db1c9fa069c20375ee6b9b19490d4da4
ArkadBabilon <skakuna32@gmail.com>
2024-01-30 13:51:57 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,40 @@
+import math
+x = 4
+print(id(x))
+#print("Hello " + "world")
+#name = input("Enter your name: ")
+#print("Your name is " + name)
+#x = 5
+#y = " hello"
+#print(str(x)+y)
+#x = int(input("Введите число: "))
+#y = 5
+#print(x+y)
+#seats = false
+#money = true
+#if seats and money:
+#    print("Идем в кино")
+#else:
+#    print("Не идем")
+x = math.sqrt(986)
+#print(x)
+#print(math.sqrt(9))
+line = r"Hello\nworld\""
+#print(line)
+line = "Hello world! " * 3
+#print(line)
+text = "hello"
+substring = text[-2]
+#print(substring)
+
+x = 5
+y = "Hello"
+#print(f"{x} + {y}")
+
+x = 5
+neg = x
+print(x)
+y = 5+5
+print(y)
+z = y + 5
+print(z)
