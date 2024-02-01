c77c04ab816526d2d6da1c940539aff869ea61e1
ArkadBabilon <skakuna32@gmail.com>
2024-02-01 11:22:38 +0300

    print added

--- main.py
+++ main.py
@@ -38,17 +38,29 @@ y = 5+5
 #print(y)
 z = y + 5
 #print(z)
+
 #list = [1, 2, 3, "Hello"]
 #print(list)
 #for i in list:
 #    print(i)
+
 #for i in list:
 #    print(i, end=" ")
-cross = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-print(cross)
-print(cross[2][1])
-cross = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
-print(cross)
-print(cross[2][1], cross[1][0])
+#cross = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+#print(cross)
+#print(cross[2][1])
+#cross = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+#print(cross)
+#print(cross[2][1], cross[1][0])
+#a = 5
+#cross = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+#print(cross)
+#print(cross[2][1] + cross[1][0] + a)
 
+list = [1, 2, 3, "Hello", True, False]
+print(list)
+list[1] = 7
+print(list[0:6:2])
+for i in list:
+    print(i)
 
